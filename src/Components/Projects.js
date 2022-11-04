a0a82ba927c480fcdb7083cfeaf32bf6524c6798
“Daysha <daysha.vu@icloud.com>
2022-11-04 14:24:45 -0700

    remove backend

---
+++ src/Components/Projects.js
@@ -0,0 +1,24 @@
+const projects = [
+    {
+        "id": 1,
+        "title": "Grocery App",
+        "url": "https://github.com/dayshajones/sinatra-project",
+        },
+        {
+        "id": 2,
+        "title": "Letters to Santa",
+        "url": "https://github.com/dayshajones/letters-to-santa",
+        },
+        {
+        "id": 3,
+        "title": "Marvel Mayhem",
+        "url": "https://github.com/dayshajones/letters-to-santa",
+        },
+        {
+        "id": 4,
+        "title": "Daysha's Donations",
+        "url": "https://github.com/dayshajones/dayshas-donations-frontend",
+        }
+]
+
+export default projects
--- src/Components/ProjectsContainer.js
+++ src/Components/ProjectsContainer.js
@@ -1,30 +1,13 @@
-import React, {useEffect, useState} from "react";
-import { Container, Button } from 'semantic-ui-react'
+import React from "react";
+import { Container } from 'semantic-ui-react'
 import ProjectCard from './ProjectCard/ProjectCard'
+import projects from './Projects'
 
 const ProjectsContainer = () => {
-
-    const [projectData, setProjectsData] = useState([])
-
-    useEffect(() => {
-        fetch("http://localhost:3000/projects")
-        .then((r) => r.json())
-        .then((results) => setProjectsData(results))
-    }, [])
-
-
-    const handleSort = () => {
-        const newSorted = [...projectData].sort((a,b) => {
-            return a.title > b.title ? 1 : -1
-        })
-        setProjectsData(newSorted)
-    }
-
     
     return (
         <Container>
-        <Button onClick={handleSort}>sort</Button>
-            {projectData.map((project) => <ProjectCard {...project} key={project.id}/>)}
+            {projects.map((project) => <ProjectCard {...project} key={project.id}/>)}
         </Container>
     )
 }
